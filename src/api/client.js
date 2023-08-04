e91b5e92721b17904aeaea07f5e9acf5129b7fef
Quang Huy <quanghuy3014@gmail.com>
2023-08-04 14:21:27 +0700

    change url

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://review-film-api.onrender.com/api",
 });
 
 export default client;
